986d060156608d623fd7a3a8950edee917e1ecd9
jankovskaa <anastasija.jankovska007@gamil.com>
2023-04-23 21:45:06 +0200

    Initial commit

---
+++ src/main/java/com/example/anastasija_jankovska_193169/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.anastasija_jankovska_193169;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
---
+++ src/main/java/com/example/anastasija_jankovska_193169/HelloResource.java
@@ -0,0 +1,14 @@
+package com.example.anastasija_jankovska_193169;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
